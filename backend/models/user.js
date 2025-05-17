eec5d697d60b02015e2e656d3927cfd307654ea5
Vivek <vivekyadav138001@gmail.com>
2025-05-17 15:28:21 +0530

    default profile photo updated

--- backend/models/user.js
+++ backend/models/user.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     },
     profilephoto: {
         type: String,
-        Default: "https://1fid.com/wp-content/uploads/2022/06/no-profile-picture-4-1024x1024.jpg",
+        Default: "https://plus.unsplash.com/premium_photo-1689606093808-3cb4393248d2?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     bio: {
         type: String
